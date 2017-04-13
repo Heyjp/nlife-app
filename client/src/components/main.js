a459cc4b1d0e651c74170074f71121a564b04fb4
Jamie Player <jamieplayer@gmail.com>
2017-04-13 16:49:36 +0100

    added ajax data to front end

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -127,7 +127,12 @@ class ListContainer extends React.Component {
 
 class Address extends React.Component {
 
-
+  state = {
+    name: this.props.data.name,
+    address: [this.props.data.location.address[0], this.props.data.location.city],
+    type: this.props.data.categories.map((ele) => ele[0] + ", "),
+    rating: this.props.data.rating
+  }
 
 
 
@@ -135,10 +140,10 @@ class Address extends React.Component {
     return (
       <div className="address">
         <ul>
-          <li><h1>The Railyway</h1></li>
-          <li>25 Colbrook Close, Haywards Heath, West Sussex, RH7 9Y2</li>
-          <li>Steak House</li>
-          <li>2.5*</li>
+          <li><h1>{this.state.name}</h1></li>
+          <li>{this.state.address}</li>
+          <li>{this.state.type}</li>
+          <li>{this.state.rating}*</li>
         </ul>
       </div>
     )
